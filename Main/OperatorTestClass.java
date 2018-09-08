dd76958926483ef7394458f711f4a21055b44118
Shohan-Pramanik <shohan.pramanik38@gmail.com>
2018-09-08 22:45:11 +0600

    Added main class

---
+++ Main/OperatorTestClass.java
@@ -0,0 +1,109 @@
+package main;
+
+public class OperatorTestClass {
+
+	public int addition(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 + operand2;
+	}
+
+	public int subtraction(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 - operand2;
+	}
+
+	public int multiplication(int operand1, int operand1) {
+		// TODO Auto-generated method stub
+		return operand1 * operand2;
+	}
+
+	public int division(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 / operand2;
+	}
+
+	public int modulas(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 % operand2;
+	}
+
+	public int bitNot(int operand) {
+		// TODO Auto-generated method stub
+		return ~operand;
+	}
+
+	public int bitAnd(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 & operand2;
+	}
+
+	public int bitOr(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 | operand2;
+	}
+
+	public int bitExOr(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 ^ operand2;
+	}
+
+	public int bitShiftRight(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 >> operand2;
+	}
+
+	public int bitShiftLeft(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 << operand2;
+	}
+
+	public int bitShiftRightZero(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 >>> operand2;
+	}
+
+	public boolean equalTo(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		if(operand1 == operand2)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean notEqualTo(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		if(operand1 != operand2)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean greaterThan(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		if(operand1 > operand1)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean logicalNot(boolean operand) {
+		// TODO Auto-generated method stub
+		return !operand;
+	}
+
+	public boolean logicalAnd(boolean operand1, boolean operand2) {
+		// TODO Auto-generated method stub
+		return operand1 && operand2;
+	}
+
+	public boolean logicalOr(boolean operand1, boolean operand2) {
+		// TODO Auto-generated method stub
+		return operand1 || operand2;
+	}
+
+	public boolean logicalExOr(boolean operand1, boolean operand2) {
+		// TODO Auto-generated method stub
+		return operand1 ^ operand2;
+	}
+
+}
